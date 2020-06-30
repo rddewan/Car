ec798551a09305327badd99c4384d11588ad5f10
Richard <richarddewan@hotmail.com>
2020-06-30 21:45:26 +0700

    project created

---
+++ app/src/main/java/com/sevenpeakssoftware/richarddewan/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sevenpeakssoftware.richarddewan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
